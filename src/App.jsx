322d31c2ba7069046065aa99c19b6113333ac7d7
bukkaramya24 <bukkaramya24@gmail.com>
2026-09-25 19:13:37 +0530

    Configure app for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import './App.css'
 
 function App(){
   return (
-    <BrowserRouter>
+    <BrowserRouter  basename="/job-application-tracker">
     <Navbar />
     <Routes>
       <Route path="/" element={<Dashboard />} />
